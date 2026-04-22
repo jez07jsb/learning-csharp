051d1f44af411c07ef7122c3924d968f8f22669f
Jezreel Barbosa <jez07jsb@gmail.com>
2026-04-21 23:14:09 -0400

    First commit beta

---
+++ AlunoConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AlunoConsole;
+
+public class Program
+{
+    string FileTestPath = @"C:\Users\jez07\Downloads";
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--*--*--*--*--*--*--*--");
+
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("--*--*--*--*--*--*--*--");
+    }
+}
